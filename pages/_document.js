509154de2cabc65ecaad5ba7f6f9d7441ce3c472
Blake Mason <bmason@lsst.org>
2020-12-02 14:51:03 -0800

    [C] Aladin script tags use https vs http

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,12 @@ class CustomDocument extends Document {
       <Html lang={this.props.lang}>
         <Head>
           {/*Aladin Lite CSS style file*/}
-          <link rel="stylesheet" href="http://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css" />
+          <link rel="stylesheet" href="https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css" />
 
           {/*Aladin Lite has a dependency on the jQuery library*/}
-          <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js" charSet="utf-8"></script>
+          <script type="text/javascript" src="https://code.jquery.com/jquery-1.9.1.min.js" charSet="utf-8"></script>
           {/*Aladin Lite JS code*/}
-          <script type="text/javascript" src="http://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js" charSet="utf-8"></script>
+          <script type="text/javascript" src="https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js" charSet="utf-8"></script>
         </Head>
         <body>
           stuff
