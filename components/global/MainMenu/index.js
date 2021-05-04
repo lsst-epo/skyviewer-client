b01442c5c0022e553589c0ad0d4ca792e4bf13d2
Blake Mason <bmason@lsst.org>
2021-05-04 11:15:34 -0700

    [F] PrimaryHeader close button responds to open menus
    If there are any modal-type menus open the close button in the PrimaryHeader hides itself to avoid any confusion with having two close buttons in screen (e.g. the one in the header and the one in the modal menu)

--- components/global/MainMenu/index.js
+++ components/global/MainMenu/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import PropTypes from "prop-types";
 import Button from "@/primitives/Button";
