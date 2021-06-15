2c2e945dbb546e3f8b066c3688e621228c973539
Blake Mason <bmason@lsst.org>
2021-06-15 10:36:58 -0700

    [C] clientside graphQL request example

--- pages/explorer/index.js
+++ pages/explorer/index.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { withLayout } from "@moxy/next-layout";
 import PrimaryLayout from "@/layouts/Primary";
 import ExplorerLayout from "@/layouts/Explorer";
-import { getCatalogsData } from "@/lib/api/catalogs";
+import { getCatalogsData, useCatalogsData } from "@/lib/api/catalogs";
 import Explorer from "@/components/explorer/index.js";
 
 // other working survey url
@@ -14,8 +14,11 @@ import Explorer from "@/components/explorer/index.js";
 // "http://localhost:5000/images"
 
 const ExplorerPage = ({ catalogs }) => {
+  const data = useCatalogsData();
   // eslint-disable-next-line no-console
-  console.log(catalogs);
+  console.log("serverside", catalogs);
+  // eslint-disable-next-line no-console
+  console.log("clientside", data);
 
   return (
     <Explorer
@@ -36,6 +39,7 @@ export default withLayout(
 )(ExplorerPage);
 
 export async function getStaticProps() {
+  // if statement is temporary until confident there will always be a server to query
   if (process.env.NEXT_PUBLIC_API_URL) {
     const { catalogs } = await getCatalogsData();
     return { props: { catalogs } };
