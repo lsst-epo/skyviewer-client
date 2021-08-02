1aecf9e5837587bdfc30c6c4ceed28e53cf5eca4
Blake Mason <bmason@lsst.org>
2021-08-01 17:01:02 -0700

    [F] Catalog & Survey data is from skyviewer-api

--- pages/explorer/index.js
+++ pages/explorer/index.js
@@ -2,7 +2,8 @@ import PropTypes from "prop-types";
 import { withLayout } from "@moxy/next-layout";
 import PrimaryLayout from "@/layouts/Primary";
 import AladinLayout from "@/layouts/Aladin";
-import { getCatalogsData, useCatalogsData } from "@/lib/api/catalogs";
+// import { getCatalogsData, useCatalogsData } from "@/lib/api/catalogs";
+import { getCatalogsSurveysData } from "@/lib/api/catalogsSurveys";
 import Explorer from "@/components/explorer/index.js";
 
 // other working survey url
@@ -13,21 +14,21 @@ import Explorer from "@/components/explorer/index.js";
 // localhost survey url
 // "http://localhost:5000/images"
 
-const ExplorerPage = ({ catalogs }) => {
-  // const data = useCatalogsData();
+const ExplorerPage = ({ catalogs, survey }) => {
+  const { fov, minFov, maxFov, path, target } = survey;
   // eslint-disable-next-line no-console
   // console.log("serverside", catalogs);
   // eslint-disable-next-line no-console
-  // console.log("clientside", data);
+  // console.log("clientside", data, catalogs);
 
   return (
     <Explorer
       selector="#aladin-lite-div"
-      catalogs={null}
-      survey="http://alasky.u-strasbg.fr/DSS/DSSColor"
-      fov={60}
-      fovRange={[2, 90]}
-      target="267.0208333333 -24.7800000000"
+      catalogs={catalogs}
+      survey={path || "http://alasky.u-strasbg.fr/DSS/DSSColor"}
+      fov={fov || 60}
+      fovRange={[minFov, maxFov] || [2, 90]}
+      target={target || "267.0208333333 -24.7800000000"}
     />
   );
 };
@@ -41,12 +42,13 @@ export default withLayout(
 export async function getStaticProps() {
   // if statement is temporary until confident there will always be a server to query
   if (process.env.NEXT_PUBLIC_API_URL) {
-    const { catalogs } = await getCatalogsData();
-    return { props: { catalogs } };
+    const { catalogs, surveys } = await getCatalogsSurveysData();
+    return { props: { catalogs, survey: surveys[0] } };
   }
-  return { props: { catalogs: null } };
+  return { props: { catalogs: null, survey: null } };
 }
 
 ExplorerPage.propTypes = {
   catalogs: PropTypes.array,
+  survey: PropTypes.object,
 };
