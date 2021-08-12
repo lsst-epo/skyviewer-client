735576537854f8271fe64c27b9aa73493efa15c1
Blake Mason <bmason@lsst.org>
2021-08-12 16:58:20 -0700

    [C] Fully commit to Surveys served from Craft (no fallback)

--- pages/explorer/index.js
+++ pages/explorer/index.js
@@ -6,26 +6,14 @@ import AladinLayout from "@/layouts/Aladin";
 import { getCatalogsSurveysData } from "@/lib/api/catalogsSurveys";
 import Explorer from "@/components/explorer/index.js";
 
-// other working survey url
-// http://alasky.u-strasbg.fr/DSS/DSSColor
-// staging survey url
-// "https://epo-hips.netlify.app/images"
-
-// localhost survey url
-// "http://localhost:5000/images"
-
 const ExplorerPage = ({ catalogs, survey }) => {
   const { fov, minFov, maxFov, path, target } = survey;
-  // eslint-disable-next-line no-console
-  // console.log("serverside", catalogs);
-  // eslint-disable-next-line no-console
-  // console.log("clientside", data, catalogs);
 
   return (
     <Explorer
       selector="#aladin-lite-div"
       catalogs={catalogs}
-      survey={path || "http://alasky.u-strasbg.fr/DSS/DSSColor"}
+      survey={path}
       fov={fov || 60}
       fovRange={[minFov, maxFov] || [2, 90]}
       target={target || "267.0208333333 -24.7800000000"}
@@ -40,12 +28,8 @@ export default withLayout(
 )(ExplorerPage);
 
 export async function getStaticProps() {
-  // if statement is temporary until confident there will always be a server to query
-  if (process.env.NEXT_PUBLIC_API_URL) {
-    const { catalogs, surveys } = await getCatalogsSurveysData();
-    return { props: { catalogs, survey: surveys[0] } };
-  }
-  return { props: { catalogs: null, survey: null } };
+  const { catalogs, surveys } = await getCatalogsSurveysData();
+  return { props: { catalogs, survey: surveys[0] } };
 }
 
 ExplorerPage.propTypes = {
