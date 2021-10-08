86047baf1883b058a365c935dbca19fcc261e969
Blake Mason <bmason@lsst.org>
2021-10-08 14:48:02 -0700

    [B] jQuery & aladin script tags in app head

--- components/layouts/Aladin/index.js
+++ components/layouts/Aladin/index.js
@@ -1,32 +1,8 @@
 import PropTypes from "prop-types";
-import Head from "next/head";
 
 export default function AladinLayout({ children }) {
   return (
     <>
-      <Head>
-        {/* Aladin Lite CSS style file */}
-        <link rel="stylesheet" href="/aladin-lite/v2/aladin.css" />
-
-        {/* Aladin Lite has a dependency on the jQuery library */}
-        <script
-          type="text/javascript"
-          src="https://code.jquery.com/jquery-1.12.1.min.js"
-          charSet="utf-8"
-        ></script>
-        {/* Aladin Lite JS code */}
-        <script
-          type="text/javascript"
-          src="/aladin-lite/v2/aladin.js"
-          charSet="utf-8"
-        ></script>
-        {/* ShareThis JS code */}
-        <script
-          type="text/javascript"
-          src="https://platform-api.sharethis.com/js/sharethis.js#property=60ad29953c756700118ae97f&product=sop"
-          async="async"
-        ></script>
-      </Head>
       <main className="viewer-container">{children}</main>
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,27 @@ class CustomDocument extends Document {
     return (
       <Html lang={this.props.lang}>
         <Head>
+          {/* Aladin Lite CSS style file */}
+          <link rel="stylesheet" href="/aladin-lite/v2/aladin.css" />
+
+          {/* Aladin Lite has a dependency on the jQuery library */}
+          <script
+            type="text/javascript"
+            src="https://code.jquery.com/jquery-1.12.1.min.js"
+            charSet="utf-8"
+          ></script>
+          {/* Aladin Lite JS code */}
+          <script
+            type="text/javascript"
+            src="/aladin-lite/v2/aladin.js"
+            charSet="utf-8"
+          ></script>
+          {/* ShareThis JS code */}
+          <script
+            type="text/javascript"
+            src="https://platform-api.sharethis.com/js/sharethis.js#property=60ad29953c756700118ae97f&product=sop"
+            async="async"
+          ></script>
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
