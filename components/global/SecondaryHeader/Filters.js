d601621243fb1b5aebb19663470af6da5195dadc
Blake Mason <bmason@lsst.org>
2021-11-09 11:02:09 -0800

    [C] Refactor FiltersMenu into reusable component + styles

--- components/global/SecondaryHeader/Filters.js
+++ components/global/SecondaryHeader/Filters.js
@@ -1,9 +1,9 @@
 import { useState, useContext } from "react";
 import PropTypes from "prop-types";
-import Menu from "@/primitives/Menu";
 import Checkbox from "@/primitives/Checkbox";
 import Slider from "@/primitives/Slider";
 import IconComposer from "@/svg/IconComposer";
+import FiltersMenu from "@/global/FiltersMenu";
 
 export default function Filters({ context, defaultFilters }) {
   const menuLabelId = "guided-experience-filters-menu-label";
@@ -46,77 +46,62 @@ export default function Filters({ context, defaultFilters }) {
   };
 
   return (
-    <Menu
-      classes="filters-menu"
-      labelledbyId={menuLabelId}
-      describedbyId={menuDescriptionId}
-      headerIcon="Filters"
+    <FiltersMenu
+      {...{ menuLabelId, menuDescriptionId }}
       heading="Filtering Tool"
       subheading="Customize tours"
+      actionButtonHandler={showFiltersReset ? resetSort : null}
       openButtonOpts={{
         icon: <IconComposer icon="Filters" />,
         text: "Open Filters Modal Dialog",
         iconText: "Filter",
         classes: "guided-experience-filters-button",
       }}
-      secondaryCloseButton
-      secondaryCloseButtonOpts={{
-        text: "Apply Filters",
-        classes: "filters-menu-close-button",
-      }}
-      actionButton
-      actionButtonHandler={showFiltersReset ? resetSort : null}
-      actionButtonOpts={{
-        text: "Reset Filters",
-        classes: "filters-menu-reset-button",
-      }}
     >
-      <div className="filters-menu-content-wrapper">
-        <div className="menu-section">
-          <h3 className="menu-section-heading">Themes</h3>
-          {filters.themes && (
-            <div className="checkboxes">
-              {Object.keys(filters.themes).map((theme, i) => {
-                return (
-                  <Checkbox
-                    key={theme + i}
-                    label={theme}
-                    checked={filters.themes[theme]}
-                    toggleCallback={handleThemeFilter}
-                  />
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <div className="menu-section">
-          <h3 className="menu-section-heading">Duration and Complexity</h3>
-          {filters.characteristics && (
-            <div className="sliders">
-              {Object.keys(filters.characteristics).map((charKey, i) => {
-                const characteristic = filters.characteristics[charKey];
-                const { min, max, step, value } = characteristic;
-
-                return (
-                  <Slider
-                    key={charKey + i}
-                    label={charKey}
-                    minLabel={min.label}
-                    maxLabel={max.label}
-                    min={min.value}
-                    max={max.value}
-                    step={step}
-                    value={value}
-                    doubleHandle
-                    onChangeCallback={handleCharacteristicFilter}
-                  />
-                );
-              })}
-            </div>
-          )}
-        </div>
+      <div className="menu-section">
+        <h3 className="menu-section-heading">Themes</h3>
+        {filters.themes && (
+          <div className="checkboxes">
+            {Object.keys(filters.themes).map((theme, i) => {
+              return (
+                <Checkbox
+                  key={theme + i}
+                  label={theme}
+                  checked={filters.themes[theme]}
+                  toggleCallback={handleThemeFilter}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
-    </Menu>
+      <div className="menu-section">
+        <h3 className="menu-section-heading">Duration and Complexity</h3>
+        {filters.characteristics && (
+          <div className="sliders">
+            {Object.keys(filters.characteristics).map((charKey, i) => {
+              const characteristic = filters.characteristics[charKey];
+              const { min, max, step, value } = characteristic;
+
+              return (
+                <Slider
+                  key={charKey + i}
+                  label={charKey}
+                  minLabel={min.label}
+                  maxLabel={max.label}
+                  min={min.value}
+                  max={max.value}
+                  step={step}
+                  value={value}
+                  doubleHandle
+                  onChangeCallback={handleCharacteristicFilter}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </FiltersMenu>
   );
 }
 
