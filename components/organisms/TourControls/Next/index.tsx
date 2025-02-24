829c34775d54ec8ee421c03b2af5a4a0a6cb497a
Alexandra Goff <agoff@lsst.org>
2025-02-24 14:01:02 -0500

    feat: tour flow

--- components/organisms/TourControls/Next/index.tsx
+++ components/organisms/TourControls/Next/index.tsx
@@ -1,31 +1,18 @@
 "use client";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { useRouter, useSearchParams } from "next/navigation";
 import { useStep } from "@/lib/tutorial";
+import { useTour } from "@/contexts/Tour";
 import TourControl from "@/components/molecules/TourControl";
 import styles from "./styles.module.css";
 
-const TourNext: FC<{ poiLimit: number }> = ({ poiLimit }) => {
+const TourNext: FC = () => {
   const { id, active } = useStep(3);
   const { t } = useTranslation();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
-  const poiParam = searchParams.get("poi");
-  const poi = poiParam ? parseInt(poiParam) : undefined;
+  const { nextPoi } = useTour();
 
   const handleNext = () => {
-    if (poi && poi < poiLimit) {
-      const position = poi + 1;
-      params.set("poi", position.toString());
-    } else if (poi && poi >= poiLimit) {
-      router.push("/");
-    } else {
-      params.set("poi", "1");
-    }
-
-    router.push(`?${params.toString()}`);
+    nextPoi();
   };
 
   return (
