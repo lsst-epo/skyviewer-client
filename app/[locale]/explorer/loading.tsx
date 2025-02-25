b7fd4f0338b49e46736c421bafd5110dbbd682ca
Alexandra Goff <agoff@lsst.org>
2025-02-25 15:23:31 -0500

    refactor: remove explorer loader

--- app/[locale]/explorer/loading.tsx
+++
@@ -1,8 +0,0 @@
-import LoadingSpinner from "@/components/primitives/LoadingSpinner";
-import { FunctionComponent } from "react";
-
-const ExplorerLoading: FunctionComponent = () => {
-  return <LoadingSpinner />;
-};
-
-export default ExplorerLoading;
