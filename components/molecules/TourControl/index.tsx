0cc6290937fe02cbd550d137710dce7c1e21c662
Alexandra Goff <agoff@lsst.org>
2025-02-26 11:05:14 -0500

    refactor: tour animations independent of router state

--- components/molecules/TourControl/index.tsx
+++ components/molecules/TourControl/index.tsx
@@ -6,6 +6,7 @@ export interface TourControlProps {
   id?: string;
   isActiveTutorialStep?: boolean;
   className?: string;
+  disabled?: boolean;
 }
 
 const TourControl: FC<PropsWithChildren<TourControlProps>> = ({
