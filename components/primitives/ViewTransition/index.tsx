c40d57f150b6ade2449af1bbf505c87f9d4650f5
Alexandra Moskowitz <agoff@lsst.org>
2025-03-07 12:21:39 -0500

    style: blur transitions

--- components/primitives/ViewTransition/index.tsx
+++ components/primitives/ViewTransition/index.tsx
@@ -1,21 +1,32 @@
+import "./transitions.css";
 import { FC, PropsWithChildren } from "react";
 import clsx from "clsx/lite";
 import styles from "./styles.module.css";
 
 interface ViewTransitionProps {
   name?: string;
+  blur?: boolean;
   className?: string;
 }
 
 const ViewTransition: FC<PropsWithChildren<ViewTransitionProps>> = ({
   name,
+  blur = true,
   className,
   children,
 }) => {
   return (
     <>
+      {blur && (
+        <style>
+          {`::view-transition-image-pair(${name}) {
+              animation-name: via-blur;
+            }`}
+        </style>
+      )}
       <div
         style={{ "--view-transition-name": name }}
+        data-blur={blur}
         className={clsx(styles.transition, className)}
       >
         {children}
