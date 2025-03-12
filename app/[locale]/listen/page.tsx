a1cf8a147a0909c97e89ea44631d230b258f6735
Alexandra Moskowitz <agoff@lsst.org>
2025-03-12 15:11:49 -0400

    feat: pixel reader demo

---
+++ app/[locale]/listen/page.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import AladinTemplate from "@/components/templates/Aladin";
+import Listener from "@/components/organisms/Listener";
+import { notFound } from "next/navigation";
+import { initialPosition } from "@/lib/helpers";
+import { getExplorerPage } from "@/services/api/explorer";
+
+const ListenerPage: FC<WithSearchParams<RootProps>> = async ({
+  params: { locale },
+  searchParams = {},
+}) => {
+  const data = await getExplorerPage(locale);
+
+  if (!data) {
+    notFound();
+  }
+
+  const { survey } = data;
+  const { fovRange, path, imgFormat, cooFrame, maxOrder, tileSize } = survey;
+
+  const options: AladinOptions = {
+    ...initialPosition(searchParams, survey),
+    backgroundColor: "rgb(0,0,0)",
+  };
+
+  return (
+    <AladinTemplate
+      fovRange={fovRange}
+      hipsConfig={{
+        id: path,
+        options: { imgFormat, cooFrame, tileSize, maxOrder },
+      }}
+      {...{ options }}
+    >
+      <Listener />
+    </AladinTemplate>
+  );
+};
+
+export default ListenerPage;
