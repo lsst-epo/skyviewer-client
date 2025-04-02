afb4e4d1d626e97dafa0f2a31abc031aa511be29
Alexandra Moskowitz <agoff@lsst.org>
2025-04-02 16:56:31 -0400

    refactor: remove inactive features

--- components/molecules/ExplorerControls/index.tsx
+++ components/molecules/ExplorerControls/index.tsx
@@ -1,52 +1,15 @@
 "use client";
-import { useState } from "react";
-import { IoIosGlobe } from "react-icons/io";
-import Filters from "./Filters";
+import { FC } from "react";
+import AladinOverlay from "@/components/atomic/AladinOverlay";
 import Share from "./Share";
 import Pan from "./Pan";
 import Zoom from "../Controls/Zoom";
-import IconToggle from "@/components/atomic/IconToggle";
-import { useAladin } from "@/contexts/Aladin";
-import AladinOverlay from "@/components/atomic/AladinOverlay";
-import styles from "./styles.module.css";
 import Orientation from "./Orientation";
-import IconComposer from "@/components/svg/IconComposer";
 import ToggleGrid from "./ToggleGrid";
 import ControlStack from "../Controls/Stack";
+import styles from "./styles.module.css";
 
-export default function Controls() {
-  const { aladin } = useAladin();
-
-  const [markersEnabled, setMarkersEnabled] = useState<boolean>(true);
-  const [landmarksEnabled, setLandmarksEnabled] = useState<boolean>(true);
-
-  const handleMarkerToggle = () => {
-    if (aladin) {
-      aladin.getOverlays().forEach((catalog) => {
-        markersEnabled ? catalog.hide() : catalog.show();
-      });
-
-      const landmarks = !markersEnabled;
-
-      setMarkersEnabled((markersEnabled) => !markersEnabled);
-      setLandmarksEnabled(landmarks);
-    }
-  };
-
-  const handleLandmarksToggle = () => {
-    if (aladin) {
-      const landmarks = aladin.getOverlays().find(({ name }) => {
-        return name === "landmark";
-      });
-
-      if (!landmarks) return;
-
-      landmarksEnabled ? landmarks.hide() : landmarks.show();
-
-      setLandmarksEnabled((landmarksEnabled) => !landmarksEnabled);
-    }
-  };
-
+const ExplorerControls: FC = () => {
   return (
     <AladinOverlay
       className="controls"
@@ -56,29 +19,17 @@ export default function Controls() {
         <Zoom />
       </ControlStack>
       <Pan />
-
-      <ControlStack position="middle left">
-        <IconToggle
-          icon={<IconComposer icon="Pin" />}
-          isChecked={landmarksEnabled}
-          onToggleCallback={handleLandmarksToggle}
-        />
-      </ControlStack>
       <ControlStack position="bottom left">
         <Share />
       </ControlStack>
       <ControlStack className={styles.viewControls} position="top right">
         <Orientation />
-        <IconToggle
-          icon={<IoIosGlobe />}
-          isChecked={markersEnabled}
-          onToggleCallback={handleMarkerToggle}
-        />
         <ToggleGrid />
-      </ControlStack>
-      <ControlStack position="top left">
-        <Filters />
       </ControlStack>
     </AladinOverlay>
   );
-}
+};
+
+ExplorerControls.displayName = "Molecule.ExplorerControls";
+
+export default ExplorerControls;
