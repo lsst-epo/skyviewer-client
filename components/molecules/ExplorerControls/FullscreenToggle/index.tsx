b487f8198b23e6cbff35c65b127427e094629511
Alexandra Moskowitz <agoff@lsst.org>
2025-04-22 16:09:06 -0400

    feat: fullscreen toggle

---
+++ components/molecules/ExplorerControls/FullscreenToggle/index.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { FC, useCallback, useState } from "react";
+import screenfull from "screenfull";
+import { useTranslation } from "react-i18next";
+import { IoMdExpand, IoIosClose } from "react-icons/io";
+import { useAladin } from "@/contexts/Aladin";
+import IconButton from "@/components/atomic/IconButton";
+
+interface FullscreenToggleProps {
+  className?: string;
+}
+
+const FullscreenToggle: FC<FullscreenToggleProps> = ({ className }) => {
+  const { t } = useTranslation();
+  const [isFullscreen, setFullscreen] = useState(false);
+  const { isLoading, aladin } = useAladin();
+
+  const onFullscreenChange = useCallback(() => {
+    setFullscreen(screenfull.isFullscreen);
+  }, []);
+
+  const toggleFullscreen = () => {
+    if (!isLoading && screenfull.isEnabled) {
+      if (screenfull.isFullscreen) {
+        screenfull.exit().then(() => {
+          screenfull.off("change", onFullscreenChange);
+          setFullscreen(false);
+        });
+      } else {
+        screenfull
+          .request(aladin.view.aladinDiv.parentElement ?? undefined)
+          .then(() => {
+            screenfull.on("change", onFullscreenChange);
+            setFullscreen(true);
+          });
+      }
+    }
+  };
+
+  return (
+    <IconButton
+      icon={isFullscreen ? <IoIosClose /> : <IoMdExpand />}
+      text={t("controls.fullscreen", {
+        context: isFullscreen ? "close" : "open",
+      })}
+      onClick={toggleFullscreen}
+      disabled={isLoading}
+      className={className}
+    />
+  );
+};
+
+FullscreenToggle.displayName = "Molecule.ExplorerControl.FullscreenToggle";
+
+export default FullscreenToggle;
