3dd13add4fefe66ded07cfbcfb00457dc69aeddd
AndrewSantaguida <andrewsantaguida@gmail.com>
2025-06-11 09:39:45 -0400

    feat: added slider and return to home button

--- components/molecules/SonificationControls/index.tsx
+++ components/molecules/SonificationControls/index.tsx
@@ -1,22 +1,50 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
+import HorizontalSlider from "@rubin-epo/epo-react-lib/HorizontalSlider";
 import Zoom from "../Controls/Zoom";
 import ControlStack from "../Controls/Stack";
+import ReturnToInitial from "../ExplorerControls/ReturnToInitial";
 import PlayPauseButton from "./PlayPauseButton/index";
+import parameters from "@/components/organisms/Listener/parameters";
 import AladinOverlay from "@/components/atomic/AladinOverlay";
+import styles from "./styles.module.css";
 
 const SonificationControls: FC = () => {
+  const [sliderValue, setSliderValue] = useState(0.5);
+
   return (
     <AladinOverlay
       className="controls"
       space="var(--size-spacing-xs) var(--size-spacing-s) var(--size-spacing-xs) var(--size-spacing-xs)"
     >
-      <ControlStack position="top right">
-        <PlayPauseButton />
-      </ControlStack>
-      <ControlStack position="middle right">
-        <Zoom />
-      </ControlStack>
+      <div className={styles.controlsWrapper}>
+        <ControlStack position="top right">
+          <PlayPauseButton />
+        </ControlStack>
+        <ControlStack position="middle right">
+          <div className={styles.centeredControls}>
+            <ReturnToInitial />
+            <Zoom />
+          </div>
+        </ControlStack>
+        <ControlStack position="bottom right">
+          <HorizontalSlider
+            styleAs="dark"
+            min={0.1}
+            max={1}
+            step={0.1}
+            value={sliderValue}
+            onChangeCallback={(value) => {
+              if (typeof value === "number") {
+                setSliderValue(value);
+                parameters.walkSpeed = value;
+                parameters.keyboardSpeed = value;
+              }
+            }}
+            className={styles.slider}
+          />
+        </ControlStack>
+      </div>
     </AladinOverlay>
   );
 };
