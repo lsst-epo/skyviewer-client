2378a9d1b85c7f5566d1c9ae87202067e8c60aed
AndrewSantaguida <andrewsantaguida@gmail.com>
2025-06-15 20:42:55 -0400

    feat: ui for the sonification app

--- components/molecules/SonificationControls/index.tsx
+++ components/molecules/SonificationControls/index.tsx
@@ -1,22 +1,48 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import Zoom from "../Controls/Zoom";
 import ControlStack from "../Controls/Stack";
+import ReturnToInitial from "../ExplorerControls/ReturnToInitial";
 import PlayPauseButton from "./PlayPauseButton/index";
+import OrientedSlider from "./OrientedSlider";
+import parameters from "@/components/organisms/Listener/parameters";
 import AladinOverlay from "@/components/atomic/AladinOverlay";
+import styles from "./styles.module.css";
 
 const SonificationControls: FC = () => {
+  const [sliderValue, setSliderValue] = useState(parameters.walkSpeed);
+
+  const handleSliderChange = (value: number) => {
+    setSliderValue(value);
+    parameters.walkSpeed = value;
+  };
+
   return (
     <AladinOverlay
       className="controls"
       space="var(--size-spacing-xs) var(--size-spacing-s) var(--size-spacing-xs) var(--size-spacing-xs)"
     >
-      <ControlStack position="top right">
-        <PlayPauseButton />
-      </ControlStack>
-      <ControlStack position="middle right">
-        <Zoom />
-      </ControlStack>
+      <div className={styles.controlsWrapper}>
+        <ControlStack position="top right">
+          <PlayPauseButton />
+        </ControlStack>
+        <ControlStack position="middle right">
+          <div className={styles.centeredControls}>
+            <ReturnToInitial />
+            <Zoom />
+          </div>
+        </ControlStack>
+        <ControlStack position="bottom center">
+          <OrientedSlider
+            min={0.1}
+            max={1}
+            step={0.02}
+            value={sliderValue}
+            onChange={handleSliderChange}
+            orientation="horizontal"
+          />
+        </ControlStack>
+      </div>
     </AladinOverlay>
   );
 };
