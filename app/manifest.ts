4ad2ae781f1671aea34d96aa59cb57b422457597
Alexandra Moskowitz <agoff@lsst.org>
2025-06-24 15:54:44 -0400

    feat: improvement to theme colors

--- app/manifest.ts
+++ app/manifest.ts
@@ -18,19 +18,19 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
     theme_color: "#1f2121",
     orientation: "natural",
     icons: [
-      { src: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { src: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { src: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
       {
         src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      { src: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { src: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { src: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
     shortcuts: [
       { name: t("navigation.cta.explore"), url: "/explore" },
