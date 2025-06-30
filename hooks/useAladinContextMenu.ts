f66a867eefb4387c13dab0161d5af48788235879
Alexandra Moskowitz <agoff@lsst.org>
2025-06-30 11:57:19 -0400

    feat: long press for context menu

---
+++ hooks/useAladinContextMenu.ts
@@ -0,0 +1,100 @@
+"use client";
+
+import { useEventListener } from "usehooks-ts";
+import { useAladin } from "@/contexts/Aladin";
+import useAladinEvent from "./useAladinEvent";
+import { useRef } from "react";
+
+const useAladinContextMenu = ({
+  onOpen,
+  onClose,
+}: {
+  onOpen: (onOpenProps: { x: number; y: number }) => void;
+  onClose: () => void;
+}) => {
+  const longPressStart = useRef<{ x: number; y: number }>();
+  const timerRef = useRef<NodeJS.Timeout>();
+  const isLongPress = useRef<boolean>();
+  const { isLoading } = useAladin({
+    callbacks: {
+      onRightClickMove: () => undefined,
+    },
+  });
+
+  const startPressTimer = (position: { x: number; y: number }) => {
+    longPressStart.current = position;
+    isLongPress.current = false;
+    timerRef.current = setTimeout(() => {
+      isLongPress.current = true;
+      onOpen && onOpen(position);
+    }, 500);
+  };
+
+  const handleKeydown = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      return onClose && onClose();
+    }
+  };
+
+  const handleTouchEvent = ({ state, type, xy }: CanvasEventDetail) => {
+    if (type === "touchstart" && xy) {
+      onClose && onClose();
+      return startPressTimer(xy);
+    }
+
+    if (type === "touchmove") {
+      if (isLongPress.current) {
+        clearTimeout(timerRef.current);
+        return onClose && onClose();
+      } else {
+        if (xy && longPressStart.current) {
+          const drift =
+            Math.abs(xy.y - longPressStart.current.y) +
+            Math.abs(xy.x - longPressStart.current.x);
+
+          if (drift > 4) {
+            longPressStart.current = undefined;
+            clearTimeout(timerRef.current);
+          }
+        }
+
+        return;
+      }
+    }
+
+    if (type === "touchend") {
+      clearTimeout(timerRef.current);
+    }
+  };
+
+  const handleClick = ({
+    detail: { state, type, xy },
+  }: AladinEventMap["Event"]) => {
+    const touchEvents = ["touchstart", "touchend", "touchmove"];
+    const closeActions = ["click", "mousedown", "wheel"];
+
+    if (type && touchEvents.includes(type)) {
+      return handleTouchEvent({ state, type, xy });
+    }
+
+    if (type && closeActions.includes(type)) {
+      return onClose && onClose();
+    }
+
+    if (state.rightClickPressed) {
+      if (type === "mousemove") {
+        return onClose && onClose();
+      }
+
+      if (!isLoading && xy) {
+        return onOpen && onOpen(xy);
+      }
+    }
+  };
+
+  useAladinEvent("zoom.changed", onClose);
+  useAladinEvent("Event", handleClick);
+  useEventListener("keydown", handleKeydown);
+};
+
+export default useAladinContextMenu;
