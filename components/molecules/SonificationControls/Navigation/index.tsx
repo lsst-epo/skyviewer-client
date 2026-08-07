32a2a64f56dda174c77226b7f4f97e92c75d534f
Sasha Persaud <159981911+soshwosh@users.noreply.github.com>
2026-08-07 16:47:56 -0700

    Sp 394 fix skysynth navigation bug (#411)
    Co-authored-by: AndrewSantaguida <andrewsantaguida@gmail.com>

--- components/molecules/SonificationControls/Navigation/index.tsx
+++ components/molecules/SonificationControls/Navigation/index.tsx
@@ -23,28 +23,7 @@ interface NavigationProps {
   className?: string;
 }
 
-type Survey = {
-  opacity: number,
-  optionalLayer: boolean,
-  showOnLoad: true,
-  id: string,
-  title: string,
-  description: string,
-  fov?: number,
-  target: string,
-  imgFormat?: string,
-  cooFrame?: String,
-  maxOrder?: number,
-  tileSize?: number,
-  fovRange?: any
-};
-
-type SurveyLayer = {
-  id: string,
-  survey: Survey,
-};
-
-function generateDestinations(layers: SurveyLayer[]): Destination[] {
+function generateDestinations(layers: []): Destination[] {
   if (!layers?.length) return [];
 
   return (
@@ -115,14 +94,14 @@ const Navigation: FC<NavigationProps> = ({
 
   const handleDestinationClick = ({ id, ra, dec, layerId }: Destination) => {
     if (isLoading) return;
-
+    
     setSelectedId(id);
-
+    
     closeNavigation();
     // Pause the walker's movement and void/boundary tracking while we travel
     parameters.resettingPosition = true;
     parameters.selectedLayerId = layerId;
-
+    
     goToPosition({
       ra,
       dec,
